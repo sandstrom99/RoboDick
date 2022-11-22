9fbc333869aa2a5096f2b155fda1bd08a73f78e0
Balder <baldersandstrom.business@gmail.com>
2022-11-22 19:10:07 +0100

    Added image download

--- cogs/sfw.py
+++ cogs/sfw.py
@@ -2,6 +2,8 @@ import os
 import random
 import discord
 from discord.ext import commands
+import requests
+import uuid
 
 IMG_PATH = "./img/sfw/"
 
@@ -17,6 +19,22 @@ class Sfw(commands.Cog):
 
         file = discord.File(IMG_PATH + random_image)
         return await ctx.channel.send(file=file)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        channel_name = message.channel.name
+        if channel_name == "not-so-safe-channel" and len(message.attachments) > 0:
+            attachment = message.attachments[0]
+            url = attachment.url
+            img_name = attachment.filename
+            img_extention = img_name.split(".")[1]
+            if img_extention == "jpg" or img_extention == "png":
+                img_data = requests.get(url).content
+                img_path = f"{IMG_PATH}{uuid.uuid1().hex}.{img_extention}"
+                with open(img_path, "wb") as handler:
+                    handler.write(img_data)
+
+                await message.add_reaction("\U0001F44C")
 
 
 async def setup(bot):
